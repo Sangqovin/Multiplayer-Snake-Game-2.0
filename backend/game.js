ed158c0e843a77365aa9bb56a00260226121279a
unknown <dyoriyukidipa@gmail.com>
2023-07-25 18:54:02 +0700

    main

--- backend/game.js
+++ backend/game.js
@@ -66,23 +66,21 @@ function gameLoop(state) {
   playerTwo.pos.y += playerTwo.vel.y;
 
   if (
-    playerOne.pos.x < -1 ||
+    // playerOne.pos.x < -1 ||
     playerOne.pos.x > GRID_SIZE ||
-    playerOne.pos.y < -1 ||
+    // playerOne.pos.y < -1 ||
     playerOne.pos.y > GRID_SIZE
   ) {
     return 2;
   }
 
   if (
-    playerTwo.pos.x < -1 ||
+    // playerTwo.pos.x < -1 ||
     playerTwo.pos.x > GRID_SIZE ||
-    playerTwo.pos.y < -1 ||
+    // playerTwo.pos.y < -1 ||
     playerTwo.pos.y > GRID_SIZE
   ) {
-    playerTwo.pos.x !== playerTwo.prevPos.x ||
-      playerTwo.pos.y !== playerTwo.prevPos.y;
-    return 2;
+    return 1;
   }
 
   if (state.food.x === playerOne.pos.x && state.food.y === playerOne.pos.y) {
@@ -99,22 +97,22 @@ function gameLoop(state) {
     randomFood(state);
   }
 
-  // if (playerOne.vel.x || playerOne.vel.y) {
-  //   for (let cell of playerOne.snake) {
-  //     if (cell.x === playerOne.pos.x && cell.y === playerOne.pos.y) {
-  //       return 2;
-  //     }
-  //   }
+  if (playerOne.vel.x || playerOne.vel.y) {
+    for (let cell of playerOne.snake) {
+      // if (cell.x === playerOne.pos.x && cell.y === playerOne.pos.y) {
+      //   return 2;
+      // }
+    }
 
-  //   playerOne.snake.push({ ...playerOne.pos });
-  //   playerOne.snake.shift();
-  // }
+    playerOne.snake.push({ ...playerOne.pos });
+    playerOne.snake.shift();
+  }
 
   if (playerTwo.vel.x || playerTwo.vel.y) {
     for (let cell of playerTwo.snake) {
-      if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
-        return 1;
-      }
+      // if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
+      //   return 1;
+      // }
     }
 
     playerTwo.snake.push({ ...playerTwo.pos });
